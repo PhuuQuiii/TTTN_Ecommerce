27298056304baa8eef3740c2b4a3911e044f8789
Nav <vunhatanh9597@gmail.com>
2025-01-02 13:15:45 +0700

    fix orderstatus

--- CRM/src/components/pages/Order/Table.js
+++ CRM/src/components/pages/Order/Table.js
@@ -1,61 +1,58 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react'
-import { Table as AntdTable, Input, Button, Space, Modal , Avatar, Drawer} from 'antd';
-import Highlighter from 'react-highlight-words';
-import moment from 'moment'
+import React, { useState, useEffect, useMemo, useRef } from 'react';
+import { Table as AntdTable, Input, Button, Space, Avatar, Drawer } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { getOrders, getOrder } from '../../../redux/actions/order_actions'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getOrders, getOrder } from '../../../redux/actions/order_actions';
 import OrderDetail from './OrderDetail';
+import moment from 'moment';
 
 const Table = ({ getOrder, getOrders, multiLoading, orders, totalCount, user }) => {
     const [pagination, setPagination] = useState({
         total: 0,
         defaultPageSize: 5,
-        pageSizeOptions:[5,10,15,20,50,100],
+        pageSizeOptions: [5, 10, 15, 20, 50, 100],
         showQuickJumper: true,
         showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
-    })
-    // const [searchText, setSearchText] = useState('')
-    // const [searchedColumn, setSearchedColumn] = useState('')
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+    });
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
     const searchInput = useRef(null);
-    useEffect(()=>{
-        setPagination({
-            ...pagination,
-            total: totalCount
-        })
-    }, [totalCount])
 
     useEffect(() => {
-        user && getOrders(user._id, pagination.current, pagination.pageSize)
-    }, [user])
+        setPagination((prevPagination) => ({
+            ...prevPagination,
+            total: totalCount
+        }));
+    }, [totalCount]);
 
+    useEffect(() => {
+        if (user) {
+            getOrders(user._id, pagination.current, pagination.pageSize);
+        }
+    }, [user, pagination.current, pagination.pageSize, getOrders]);
 
     const handleTableChange = (pagination, filters) => {
-        console.log(pagination);
-        user && getOrders(user._id, pagination.current, pagination.pageSize, filters.status?.[0],filters.product?.[0])
-    }
+        if (user) {
+            getOrders(user._id, pagination.current, pagination.pageSize, filters.status?.[0], filters.product?.[0]);
+        }
+    };
 
     const handleSearch = (selectedKeys, confirm, dataIndex) => {
         confirm();
-        // setSearchText(selectedKeys[0])
-        // setSearchedColumn(dataIndex)
     };
 
-    const handleReset = clearFilters => {
+    const handleReset = (clearFilters) => {
         clearFilters();
-        // setSearchText('')
     };
 
-    const getOrderSearchProps = dataIndex => ({
+    const getOrderSearchProps = (dataIndex) => ({
         filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-            <div style={{ padding: 8, backgroundColor: '#495057'}}>
+            <div style={{ padding: 8, backgroundColor: '#495057' }}>
                 <Input
                     ref={searchInput}
                     placeholder={`Search ${dataIndex}`}
                     value={selectedKeys[0]}
-                    onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
                     onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
                     style={{ width: 188, marginBottom: 8, display: 'block' }}
                 />
@@ -75,39 +72,24 @@ const Table = ({ getOrder, getOrders, multiLoading, orders, totalCount, user }) 
                 </Space>
             </div>
         ),
-        filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : '#495057' }} />,
-        // onFilter: (value, record) =>
-        //     record[dataIndex]
-        //         ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
-        //         : '',
-        onFilterDropdownVisibleChange: visible => {
+        filterIcon: (filtered) => <SearchOutlined style={{ color: filtered ? '#1890ff' : '#495057' }} />,
+        onFilterDropdownVisibleChange: (visible) => {
             if (visible) {
                 setTimeout(() => searchInput.current.select(), 100);
             }
         },
-        render: product =>{
-            return (<>
-                {product.name}{' '}<Avatar shape="square" size='small' src={`${process.env.REACT_APP_SERVER_URL}/uploads/${product.images[0].thumbnail}`}/>
-            </>)
-
-            // return searchedColumn === dataIndex ? (
-            //     <Highlighter
-            //         highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-            //         searchWords={[searchText]}
-            //         autoEscape
-            //         textToHighlight={text ? text.name.toString() : ''}
-            //     />
-            // ) : (
-            //         text.name
-            //     )
-        }
-        
-    })
+        render: (product) => (
+            <>
+                {product.name}{' '}
+                <Avatar shape="square" size="small" src={`${process.env.REACT_APP_SERVER_URL}/uploads/${product.images[0].thumbnail}`} />
+            </>
+        )
+    });
 
     const openOrder = (order) => {
-        setIsDrawerOpen(true)
-        getOrder(order.soldBy, order._id)
-    }
+        setIsDrawerOpen(true);
+        getOrder(order.soldBy, order._id);
+    };
 
     const columns = useMemo(() => [
         {
@@ -119,7 +101,7 @@ const Table = ({ getOrder, getOrders, multiLoading, orders, totalCount, user }) 
         {
             title: 'Customer',
             dataIndex: 'user',
-            render: user => `${user.name}`,
+            render: (user) => `${user.name}`,
             width: '10%',
         },
         {
@@ -129,21 +111,21 @@ const Table = ({ getOrder, getOrders, multiLoading, orders, totalCount, user }) 
                     title: 'City',
                     dataIndex: 'shipto',
                     key: 'city',
-                    render: shipto => `${shipto.city}`,
+                    render: (shipto) => `${shipto.city}`,
                     width: '10%',
                 },
                 {
                     title: 'Area',
                     dataIndex: 'shipto',
                     key: 'area',
-                    render: shipto => `${shipto.area}`,
+                    render: (shipto) => `${shipto.area}`,
                     width: '10%',
                 },
                 {
                     title: 'Street',
                     dataIndex: 'shipto',
                     key: 'address',
-                    render: shipto => `${shipto.address}`,
+                    render: (shipto) => `${shipto.address}`,
                     width: '10%',
                 },
             ]
@@ -162,15 +144,15 @@ const Table = ({ getOrder, getOrders, multiLoading, orders, totalCount, user }) 
                 { text: 'Tobereturned', value: 'tobereturned' },
                 { text: 'Return', value: 'return' },
             ],
-            render: status => {
+            render: (status) => {
                 let badgeClass = status.currentStatus === 'active' ? "badge badge-pill badge-info" :
-                status.currentStatus === 'approve' ? "badge badge-pill badge-secondary" :
-                status.currentStatus === 'dispatch' ? "badge badge-pill badge-primary":
-                status.currentStatus === 'cancel' ? "badge badge-pill badge-danger":
-                status.currentStatus === 'complete' ? "badge badge-pill badge-success":
-                status.currentStatus === 'tobereturned' ? "badge badge-pill badge-warning":
-                "badge badge-pill badge-dark"
-                return(<span className={badgeClass}>{status.currentStatus}</span>)
+                    status.currentStatus === 'approve' ? "badge badge-pill badge-secondary" :
+                        status.currentStatus === 'dispatch' ? "badge badge-pill badge-primary" :
+                            status.currentStatus === 'cancel' ? "badge badge-pill badge-danger" :
+                                status.currentStatus === 'complete' ? "badge badge-pill badge-success" :
+                                    status.currentStatus === 'tobereturned' ? "badge badge-pill badge-warning" :
+                                        "badge badge-pill badge-dark";
+                return (<span className={badgeClass}>{status.currentStatus}</span>);
             },
             width: '10%',
         },
@@ -183,58 +165,47 @@ const Table = ({ getOrder, getOrders, multiLoading, orders, totalCount, user }) 
             title: 'Date',
             dataIndex: 'createdAt',
             width: '9%',
-            render: date => `${moment(date).format("MMM Do YYYY")}`
+            render: (date) => `${moment(date).format("MMM Do YYYY")}`
         },
         {
             title: 'Action',
             dataIndex: '',
             width: '6%',
-            render: action => <button className="btn btn-info btn-sm"><i className="fas fa-eye"></i></button>,
-            onCell:order => {
+            render: (action) => <button className="btn btn-info btn-sm"><i className="fas fa-eye"></i></button>,
+            onCell: (order) => {
                 return {
-                    onClick: e => openOrder(order)
-                }
+                    onClick: (e) => openOrder(order)
+                };
             }
         },
-    ], []);
-    
+    ], [user, getOrder]);
 
     return (
-    <>
-    <AntdTable
-        columns={columns}
-        rowKey={record => record._id}
-        dataSource={orders}
-        pagination={pagination}
-        loading={multiLoading}
-        onChange={handleTableChange}
-        size='small'
-        sticky
-        // scroll={{ y: 400 }}
-    />
-    <Drawer
-        title="Order Details"
-        placement="right"
-        width={800}
-        closable
-        onClose={()=>setIsDrawerOpen(false)}
-        visible={isDrawerOpen}
-        closeIcon={<i className="fas fa-times btn btn-danger"></i>}
-    >
-        <OrderDetail isOrderDetailOpen={isDrawerOpen} />
-    </Drawer>
-    {/* <Modal
-        title="Order Detail"
-        centered
-        visible={isDrawerOpen}
-        onOk={() => setIsDrawerOpen(false)}
-        onCancel={() => setIsDrawerOpen(false)}
-        width={1000}
-    >
-        <OrderDetail order={order}/>
-    </Modal> */}
-    </>)
-}
+        <>
+            <AntdTable
+                columns={columns}
+                rowKey={(record) => record._id}
+                dataSource={orders}
+                pagination={pagination}
+                loading={multiLoading}
+                onChange={handleTableChange}
+                size='small'
+                sticky
+            />
+            <Drawer
+                title="Order Details"
+                placement="right"
+                width={800}
+                closable
+                onClose={() => setIsDrawerOpen(false)}
+                visible={isDrawerOpen}
+                closeIcon={<i className="fas fa-times btn btn-danger"></i>}
+            >
+                <OrderDetail isOrderDetailOpen={isDrawerOpen} />
+            </Drawer>
+        </>
+    );
+};
 
 Table.propTypes = {
     user: PropTypes.object,
@@ -243,12 +214,13 @@ Table.propTypes = {
     pageCount: PropTypes.number,
     getOrder: PropTypes.func.isRequired,
     getOrders: PropTypes.func.isRequired,
-}
+};
+
 const mapStateToProps = (state) => ({
     user: state.auth.adminProfile,
     orders: state.order.orders,
     multiLoading: state.order.multiLoading,
     totalCount: state.order.totalCount,
-})
+});
 
-export default connect(mapStateToProps, { getOrders, getOrder })(Table)
+export default connect(mapStateToProps, { getOrders, getOrder })(Table);
