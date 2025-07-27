19fd907659fa83c199e39b7241b6e8bd74d28bbc
PhuuQuiii <nguyenphuqui170304@gmail.com>
2025-07-27 12:05:07 +0700

    sửa productSerrvice.js và constant.js ở client

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,15 +1,18 @@
-export const QUINDIGO_BASE_URL = 'http://157.245.106.101'
+export const QUINDIGO_BASE_URL = "http://157.245.106.101";
 
 // export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://157.245.106.101:3001' : process.env.SERVER_BASE_URL
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://backend-ecommerce-theta-plum.vercel.app' : process.env.SERVER_BASE_URL
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://backend-ecommerce-theta-plum.vercel.app"
+    : process.env.SERVER_BASE_URL;
 
-export const USER_AUTH_BASE_URL = `${BASE_URL}/api/user-auth`
-export const SUPER_AUTH_BASE_URL = `${BASE_URL}/api/superadmin`
-export const USER_BASE_URL = `${BASE_URL}/api/user`
-export const CART_BASE_URL = `${BASE_URL}/api/cart-wishlist`
-export const ORDER_BASE_URL = `${BASE_URL}/api/order`
-export const PRODUCT_BASE_URL = `${BASE_URL}/api/product`
-export const REVIEW_BASE_URL = `${BASE_URL}/api/review-qna`
-export const WISHLIST_BASE_URL = `${BASE_URL}/api/cart-wishlist`
-export const IMAGE_BASE_URL = `${BASE_URL}/uploads`
-export const SALE_BASE_URL = `${BASE_URL}/api/sale`
+export const USER_AUTH_BASE_URL = `${BASE_URL}/user-auth`;
+export const SUPER_AUTH_BASE_URL = `${BASE_URL}/superadmin`;
+export const USER_BASE_URL = `${BASE_URL}/user`;
+export const CART_BASE_URL = `${BASE_URL}/cart-wishlist`;
+export const ORDER_BASE_URL = `${BASE_URL}/order`;
+export const PRODUCT_BASE_URL = `${BASE_URL}/product`;
+export const REVIEW_BASE_URL = `${BASE_URL}/review-qna`;
+export const WISHLIST_BASE_URL = `${BASE_URL}/cart-wishlist`;
+export const IMAGE_BASE_URL = `${BASE_URL}/uploads`;
+export const SALE_BASE_URL = `${BASE_URL}/sale`;
